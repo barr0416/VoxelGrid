984e076faaa842335b4ec2bd330b0f43f8adffad
barr0416 <codydbarr@gmail.com>
2015-02-27 15:02:14 -0500

    Added chunks allocation.
    - Added chunks allocation through a voxel map controller
    - Updated the voxel grid with an initialize function

--- VoxelGrid/Assets/VoxelGrid.cs
+++ VoxelGrid/Assets/VoxelGrid.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+//We want to be able to select the entire grid, so this will allow it by default
+[SelectionBase]
 public class VoxelGrid : MonoBehaviour {
 
 	//The size of the grid
@@ -15,21 +17,27 @@ public class VoxelGrid : MonoBehaviour {
 	//The size of the voxel
 	private float voxelSize;
 	
-	private void Awake() {
+	//Space between objects
+	private float voxelSpacer = 0.95f;
+	
+	public void Initialize(int resolution, float size) {
+		this.m_GridSize = resolution;
 		//Set the size of the voxel
-		voxelSize = 1.0f / m_GridSize;
+		voxelSize = size / m_GridSize;
 		
 		//Set the grid size
-		voxels = new bool[m_GridSize * m_GridSize];
+		voxels = new bool[resolution * resolution];
+		
+		//For the size of the grid, create a new voxel
+		for (int i = 0, y = 0; y < resolution; y++) {
+			for (int x = 0; x < resolution; x++, i++) {
+				CreateVoxel(i, x, y);
+			}
+		} 
 	}
 	
 	private void Start() {
-		//For the size of the grid, create a new voxel
-		for (int i = 0, y = 0; y < m_GridSize; y++) {
-			for (int x = 0; x < m_GridSize; x++, i++) {
-				CreateVoxel(i, x, y);
-			}
-		}
+
 	}
 	
 	/// <summary>
@@ -45,6 +53,6 @@ public class VoxelGrid : MonoBehaviour {
 		//Set the transforms
 		newVoxel.transform.parent = transform;	
 		newVoxel.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
-		newVoxel.transform.localScale = Vector3.one * voxelSize;
+		newVoxel.transform.localScale = Vector3.one * voxelSize * voxelSpacer;
 	}
 }
---
+++ VoxelGrid/Assets/VoxelMap.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VoxelMap : MonoBehaviour {
+
+	public float m_Size = 2.0f;
+	
+	public int m_VoxelResolution = 8;
+	public int m_ChunkResolution = 2;
+	
+	public VoxelGrid m_VoxelGrid;
+	private VoxelGrid[] chunks;
+	
+	private float chunkSize;
+	private float voxelSize;
+	private float halfSize;
+	
+	private void Awake() {
+		//Set the half size
+		halfSize = m_Size * 0.5f;
+		//Set the size of the chunk
+		chunkSize = m_Size / m_ChunkResolution;
+		//Set the size of the voxels
+		voxelSize = chunkSize / m_VoxelResolution;
+		
+		//For making the chunks
+		chunks= new VoxelGrid[m_ChunkResolution * m_ChunkResolution];
+		
+		//Create a new chunk
+		for(int i = 0, y = 0; y < m_ChunkResolution; y++) {
+			for(int x = 0; x < m_ChunkResolution; x++, i++) {
+				CreateChunk(i, x, y);
+			}
+		}
+	}
+	
+	private void CreateChunk (int i, int x, int y) {
+		VoxelGrid chunk = Instantiate(m_VoxelGrid) as VoxelGrid;
+		chunk.Initialize(m_VoxelResolution, chunkSize);
+		chunk.transform.parent = transform;
+		chunk.transform.localPosition = new Vector3(x * chunkSize - halfSize, y * chunkSize - halfSize);
+		chunks[i] = chunk;
+	}
+}
