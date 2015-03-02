5a97cc7e41d7604122f9ab1b2a0c29ff8aeee981
barr0416 <codydbarr@gmail.com>
2015-03-02 13:13:15 -0500

    Revert "Updated voxel material with material color rever."
    This reverts commit 0a722a0c6c38dbdc00138a0bf16c88016969f223.

--- VoxelGrid/Assets/VoxelGrid.cs
+++ VoxelGrid/Assets/VoxelGrid.cs
@@ -11,23 +11,14 @@ public class VoxelGrid : MonoBehaviour {
 	//The voxel prefab
 	public GameObject m_Prefab;
 	
-	//The materials
-	private Material[] voxelMaterials;
-	
-	//The game objects
-	private GameObject[] voxelObjects;
-	
 	//The voxels array
-	protected bool[] voxels;
+	private bool[] voxels;
 	
 	//The size of the voxel
 	private float voxelSize;
 	
 	//Space between objects
 	private float voxelSpacer = 0.95f;
-	
-	//The transparent coloring
-	private Color transparent = new Color(0.0f, 0.0f, 0.0f, 0.0f);
 	
 	public void Initialize(int resolution, float size) {
 		this.m_GridSize = resolution;
@@ -37,63 +28,16 @@ public class VoxelGrid : MonoBehaviour {
 		//Set the grid size
 		voxels = new bool[resolution * resolution];
 		
-		//Set the material 
-		voxelMaterials = new Material[voxels.Length];
-		
 		//For the size of the grid, create a new voxel
 		for (int i = 0, y = 0; y < resolution; y++) {
 			for (int x = 0; x < resolution; x++, i++) {
 				CreateVoxel(i, x, y);
 			}
 		} 
-		
-		InitializeVoxelColors();
 	}
 	
-	/// <summary>
-	/// Apply the specified x, y, state and stencil.
-	/// </summary>
-	/// <param name="x">The x coordinate.</param>
-	/// <param name="y">The y coordinate.</param>
-	/// <param name="state">If set to <c>true</c> state.</param>
-	/// <param name="stencil">Stencil.</param>
-	public void Apply(int x, int y, bool edit, VoxelStencil stencil) {	
-		if(edit) {
-			bool isOrignalColor = true;
-			int index = y * m_GridSize + x;
-			voxels[index] = stencil.Apply(x, y);
-			
-			if(voxelMaterials[index].color != Color.white) {
-				isOrignalColor = false;
-			}
-			
-			SetVoxelColors(isOrignalColor, index);
-		}
-	}
-	
-	/// <summary>
-	/// Sets the voxel colors.
-	/// </summary>
-	/// <param name="isOriginalColor">If set to <c>true</c> is original color.</param>
-	/// <param name="id">Identifier.</param>
-	private void SetVoxelColors(bool isOriginalColor, int id) {
-	
-		//If the color has been changed before, we can change it back
-		if(!isOriginalColor) {
-			voxelMaterials[id].color = Color.white;
-		}
-		else {
-			voxelMaterials[id].color = transparent;
-		}
-	}
-	
-	/// <summary>
-	/// Initializes the voxel colors.
-	/// </summary>
-	private void InitializeVoxelColors() {
-		for (int i = 0; i < voxels.Length; i++) {
-			voxelMaterials[i].color = voxels[i] ? transparent : Color.white;
-		}
+	private void Start() {
+
 	}
 	
 	/// <summary>
@@ -102,7 +46,7 @@ public class VoxelGrid : MonoBehaviour {
 	/// <param name="i">The index.</param>
 	/// <param name="x">The x coordinate.</param>
 	/// <param name="y">The y coordinate.</param>
-	private void CreateVoxel(int i, int x, int y) {
+	private void CreateVoxel (int i, int x, int y) {
 		//The new game object
 		GameObject newVoxel = Instantiate(m_Prefab) as GameObject;
 		
@@ -110,7 +54,5 @@ public class VoxelGrid : MonoBehaviour {
 		newVoxel.transform.parent = transform;	
 		newVoxel.transform.localPosition = new Vector3((x + 0.5f) * voxelSize, (y + 0.5f) * voxelSize);
 		newVoxel.transform.localScale = Vector3.one * voxelSize * voxelSpacer;
-		
-		voxelMaterials[i] = newVoxel.GetComponent<MeshRenderer>().material;
 	}
 }
--- VoxelGrid/Assets/VoxelStencil.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class VoxelStencil {
-
-	public bool Apply(int x, int y) {
-		return true;
-	}
-}
